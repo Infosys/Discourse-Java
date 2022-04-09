61fb1a1a74d39e8939449d63104e6d36c815b18d
Hitesh C <jainhitesh9998@gmail.com>
2022-04-09 21:34:26 +0530

    [ADDED] missing copyright notices

--- src/main/java/com/infy/service/impl/TopicsServiceImpl.java
+++ src/main/java/com/infy/service/impl/TopicsServiceImpl.java
@@ -1,4 +1,10 @@
 package com.infy.service.impl;
+/*
+ * Copyright 2021 Infosys Ltd.
+ * Use of this source code is governed by GNU General Public License version 2
+ * that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/GPL-2.0
+ */
 
 import java.time.Instant;
 import java.util.List;
